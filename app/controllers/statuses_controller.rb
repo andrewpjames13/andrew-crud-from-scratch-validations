b6461fe41efcf44a5035f119686d7170d37ff92e
Andrew James <andrewpjames87@gmail.com>
2015-02-20 11:29:45 -0700

    User can add a status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,29 @@
 class StatusesController < ApplicationController
 
   def index
-  
+    @statuses = Status.all
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    if @status.save
+      redirect_to new_status_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:status, :user, :likes)
   end
 
 end
