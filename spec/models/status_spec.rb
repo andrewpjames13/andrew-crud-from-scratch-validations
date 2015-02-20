3356c96d7898f5b098380639df31e4333a7b4b52
Andrew James <andrewpjames87@gmail.com>
2015-02-20 15:12:32 -0700

    Spec model for statuses

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Status, type: :model do
     expect(stat.status).to eql "teststat"
     expect(stat.user).to eql "testuser"
   end
+  it 'has a uniqe user name' do
+    old_stat = Status.create(:status => "teststat", :user => "testuser")
+    new_stat = Status.create(:status => "teststat", :user => "testuser")
+    expect(new_stat).to be_invalid
+  end
 
+  it 'can have likes' do
+    stat = Status.create(:status => "teststat", :user => "testuser", :likes => 1)
+    expect(stat.likes).to eql 1
+  end
 end
